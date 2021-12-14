ad94b921865197992c13e45ce7d0a332420d7fff
Jasmine Elkins <jasmine.elkins@gmail.com>
2021-12-14 10:44:08 -0500

    The game works!

--- script.js
+++ script.js
@@ -40,19 +40,20 @@ class Player {
     return this._health;
   }
 
-  constructor(n) {
+  constructor(n, w, t, l, h) {
+    this.reset();
     this._name = n;
   }
 
-  // add method to initialize players & start/restart the game
+  // initialize players & start/restart the game
   reset() {
     this._level = "5";
     this._type = "Peasant";
     this._weapon = "Light Saber";
-    this._health = 1;
+    this._health = 5;
   }
 
-  // add method to return current status on all properties of players
+  // return current status on all players
   getAllInfo() {
     if (parseInt(this._health) === 0) {
       this._health = this.name + " Died!";
@@ -76,6 +77,61 @@ class Player {
       );
     }
   }
+
+  doBattle(goodGuy, enemy) {
+    // determine who takes damage using random number
+    let attackByGood = this.getRandom();
+    // add damage per attack?
+    // track number of blows?
+    // depending on level, add new weapon with more damage?
+
+    let attackByEnemy = this.getRandom();
+
+    let action = document.querySelector("#action");
+
+    if (attackByGood > attackByEnemy) {
+      action.value = goodGuy.name + " attacks " + enemy.name;
+      goodGuy.health++;
+      goodGuy.level++;
+
+      enemy.health--;
+      enemy.level--;
+    } else if (attackByGood < attackByEnemy) {
+      action.value = enemy.name + " attacks " + goodGuy.name;
+      goodGuy.health--;
+      goodGuy.level--;
+
+      enemy.health++;
+      enemy.level++;
+    }
+  }
+  getRandom() {
+    let r = Math.floor(Math.random() * 10) + 1;
+    return r;
+  }
 }
 
 // set up players
+let player1 = new Player("Lea");
+let player2 = new Player("Bolivar");
+
+function initGame() {
+  player1.reset();
+  player2.reset();
+
+  showStatus();
+}
+
+function startGame() {
+  player1.doBattle(player1, player2);
+
+  showStatus();
+}
+
+function showStatus() {
+  let playerOneInfo = document.querySelector("#p1");
+  playerOneInfo.value = player1.getAllInfo();
+
+  let playerTwoInfo = document.querySelector("#p2");
+  playerTwoInfo.value = player2.getAllInfo();
+}
